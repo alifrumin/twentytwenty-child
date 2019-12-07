cc33a3065254d38a5032e0f04d869489858f4592
Alice Frumin <alifrumin@yahoo.com>
2019-12-07 01:07:23 -0500

    adding custom fields

--- template-parts/content-book.php
+++ template-parts/content-book.php
@@ -30,7 +30,7 @@
 
    <header class="entry-header <?php echo esc_attr( $entry_header_classes ); ?>">
 
-  	<div class="entry-header-inner section-inner medium">
+  	<div>
 
       <?php
 
@@ -38,7 +38,7 @@
   			the_title( '<h1 class="entry-title">', '</h1>' );
   		} else {
         ?>
-          <h3><a style="color: <?php the_field('text_color'); ?>;"href="<?php esc_url( get_permalink()); ?>">the_title();</a></h3>
+          <h3><a style="color: <?php the_field('text_color'); ?>;"href="<?php esc_url( get_permalink()); ?>"><?php the_title(); ?></a></h3>
         <?php
   		}
       ?>
